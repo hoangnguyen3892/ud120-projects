19a056d12875c967765f662b65a352075e6e51f0
Hoang Nguyen <hoangnguyen3892@gmail.com>
2017-01-27 22:24:23 +0900

    Finished feature scaling

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,9 +48,9 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
-feature_3 = "total_payments"
+#feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -82,11 +82,14 @@ max_salary = max(salary_list)
 print "Minimum salary value: {0}".format(min_salary)
 print "Maximum salary value: {0}".format(max_salary)
 
+
+
+
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2, f3 in finance_features:
+for f1, f2 in finance_features:
     plt.scatter( f1, f2)
 plt.show()
 
@@ -99,10 +102,13 @@ kmeans = KMeans(n_clusters=2, random_state=0).fit(finance_features)
 pred = kmeans.predict(finance_features)
 
 
-
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaler.fit(finance_features)
+print scaler.transform([200000., 1000000.])
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters_before_scaling.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters_scaleds.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
