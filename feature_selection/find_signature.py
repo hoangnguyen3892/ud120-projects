d7b3fbac409aad873369f4f18bf599649a4ba80b
Hoang Nguyen <hoangnguyen3892@gmail.com>
2017-03-02 23:04:28 +0900

    Finished feature selection

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -19,8 +19,8 @@ authors = pickle.load( open(authors_file, "r") )
 ### remainder go into training)
 ### feature matrices changed to dense representations for compatibility with
 ### classifier functions in versions 0.15.2 and earlier
-from sklearn import cross_validation
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
@@ -32,12 +32,38 @@ features_test  = vectorizer.transform(features_test).toarray()
 ### a classic way to overfit is to use a small number
 ### of data points and a large number of features;
 ### train on only 150 events to put ourselves in this regime
+
 features_train = features_train[:150].toarray()
 labels_train   = labels_train[:150]
-
+#features_train = features_train.toarray()
+#labels_train   = labels_train
 
 
 ### your code goes here
 
+#Quiz: Number Of Features And Overfitting
+print "Number of features: %s" %len(features_train)
+
+from sklearn.tree import DecisionTreeClassifier
+
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
 
 
+#Quiz: Accuracy Of Your Overfit Decision Tree
+from sklearn.metrics import accuracy_score
+
+print "Accuracy: %5.4f" %accuracy_score(labels_test, pred)
+
+#Quiz: Identify The Most Powerful Features
+#Quiz: Use TfIdf To Get The Most Important Word
+importances = clf.feature_importances_
+feature_num = 0
+
+for importance in importances:
+	if importance >= 0.2:
+		print feature_num, importance, vectorizer.get_feature_names()[feature_num]
+
+	feature_num += 1
--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -52,13 +52,13 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-            signature_words = ["sara", "shackleton", "chris", "germani"]
+            signature_words = ["sara", "shackleton", "chris", "germani", "sshacklensf", "cgermannsf"]
             for word in signature_words:
                 text = text.replace(word, '')
             ### append the text to word_data
             word_data.append(text)
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-            if name == "Sara":
+            ### append a 0 to from_data if email is from Sara (sara not Sara), and 1 if email is from Chris
+            if name == "sara":
                 from_data.append(0)
             else:
                 from_data.append(1)
